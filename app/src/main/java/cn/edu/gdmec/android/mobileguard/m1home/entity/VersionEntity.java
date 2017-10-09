d0325023ef628a152f67c4d16c02eb1eebbe12bb
caijunyu1997 <997021998@qq.com>
2017-10-09 18:25:23 +0800

    欢迎页面自动升级版本

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,12 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by 蔡俊宇 on 2017/9/24.
+ */
+
+public class VersionEntity {
+    //变量
+    public String versionCode;
+    public String description;
+    public String apkurl;
+}
