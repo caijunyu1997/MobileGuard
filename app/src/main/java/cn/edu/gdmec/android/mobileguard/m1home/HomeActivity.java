705afe7c00279148bb8a30cb580ba604b3157c2f
caijunyu1997 <997021998@qq.com>
2017-11-05 19:44:21 +0800

    通讯卫士模块

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -21,6 +21,7 @@ import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetUpPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.receiver.MyDeviceAdminReceiver;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
+import cn.edu.gdmec.android.mobileguard.m3communicationguard.SecurityPhoneActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -53,6 +54,9 @@ public class HomeActivity extends AppCompatActivity {
                             //弹出设置密码框
                             showSetUpPswDialog();
                         }
+                        break;
+                    case 1://点击通讯卫士
+                        startActivity(SecurityPhoneActivity.class);
                         break;
                 }
             }
